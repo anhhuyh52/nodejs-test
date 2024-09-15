84b567f3041510270e7a1133f5d3fc283da8230a
anhhuyh52 <anhhuyd3v@gmail.com>
2024-09-16 02:22:12 +0700

    Update connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const compression = require('compression')
+const helmet = require('helmet')
 const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
+
+require('./dbs/init.mongodb')
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({ message: "Welcome boysssss" });
